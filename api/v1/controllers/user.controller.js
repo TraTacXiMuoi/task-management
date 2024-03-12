7f5ed3d47cb73af0edc93ea3a409fb5d321353e8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-12 21:48:38 +0700

    6. Lưu những người tham gia vào task

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -174,3 +174,16 @@ module.exports.detail = async (req, res) => {
     info: res.locals.user
   });
 };
+
+// [POST] /api/v1/users/list
+module.exports.list = async (req, res) => {
+  const users = await User.find({
+    deleted: false
+  }).select("id fullName email");
+
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    users: users
+  });
+};
